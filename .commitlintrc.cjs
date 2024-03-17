286b86583c85091ee42c2a1dc8f065c5fa6355ba
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2024-03-17 17:22:33 +0100

    chore: fix commitlint in ci

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,10 +1,5 @@
 const Configuration = {
   extends: ['@commitlint/config-conventional'],
-  ignores: [
-    (commit) => {
-      return /^(first commit|project init)/.test(commit);
-    },
-  ],
 };
 
 module.exports = Configuration;
