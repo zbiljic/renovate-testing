9d89fc9712168c946c7289ffa72ee8f5fb8044f1
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2024-03-25 19:30:20 +0100

    chore: update Renovate configuration

--- .github/renovate-config.cjs
+++ .github/renovate-config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   onboarding: false,
   dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
-  // username: 'renovate[bot]',
+  username: 'renovate[bot]',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   allowedPostUpgradeCommands: ['.*'],
 };
